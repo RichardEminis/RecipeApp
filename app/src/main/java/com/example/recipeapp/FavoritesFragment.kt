6480ec74a52aebef777ce746d6d4911d809ee7a5
Alexandr Belyaev <eminis.ru@gmail.com>
2024-01-07 00:36:51 +0300

    RA-05 Create FavoritesFragment and add ClickListener

---
+++ app/src/main/java/com/example/recipeapp/FavoritesFragment.kt
@@ -0,0 +1,25 @@
+package com.example.recipeapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipeapp.databinding.FragmentFavoritesBinding
+import com.example.recipeapp.databinding.FragmentListCategoriesBinding
+
+class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}

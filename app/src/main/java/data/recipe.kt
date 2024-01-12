09d42966087a333b020e09776bfbddc13e13dca4
Alexandr Belyaev <eminis.ru@gmail.com>
2024-01-13 00:25:30 +0300

    RA-06 Edit fun getCategories

--- app/src/main/java/data/recipe.kt
+++ app/src/main/java/data/recipe.kt
@@ -13,7 +13,7 @@ object STUB {
         Category(5, "Салаты", "Хрустящий калейдоскоп под соусом вдохновения", "salad.png")
     ))
 
-    fun getCategories() {
-        println(categories)
+    fun getCategories(): List<Category> {
+        return categories
     }
 }

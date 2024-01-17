633ab7ad906af402bd0733c9e391bae0572a5b7f
Alexandr Belyaev <eminis.ru@gmail.com>
2024-01-17 19:53:07 +0300

    RA-07 Edit RecyclerView add tool parameter

--- app/src/main/java/com/example/recipeapp/CategoriesCardAdapter.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesCardAdapter.kt
@@ -4,7 +4,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class CategoriesCardAdapter: RecyclerView.Adapter<CategoriesCardAdapter.ViewHolder>() {
+class CategoriesCardAdapter : RecyclerView.Adapter<CategoriesCardAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     }

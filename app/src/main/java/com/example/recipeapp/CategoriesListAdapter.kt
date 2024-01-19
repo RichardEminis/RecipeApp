c41a6d98dd2aea4650f28d6f9738fcc62d4d0aeb
Alexandr Belyaev <eminis.ru@gmail.com>
2024-01-19 18:57:56 +0300

    RA-07 Create Adapter and add items to ViewHolder, Edit ID items

---
+++ app/src/main/java/com/example/recipeapp/CategoriesListAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.recipeapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import model.Category
+
+class CategoriesListAdapter(
+    private val dataSet: Category,
+    private val fragment: CategoriesListFragment
+) : RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val categoryItem: CardView = itemView.findViewById(R.id.cvCategoryItem)
+        val categoryImage: ImageView = itemView.findViewById(R.id.ivCategoryImage)
+        val categoryName: TextView = itemView.findViewById(R.id.tvCategoryName)
+        val categoryDescription: TextView = itemView.findViewById(R.id.tvCategoryDescription)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoriesListAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
+
+        return TODO()
+    }
+
+    override fun onBindViewHolder(holder: CategoriesListAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}

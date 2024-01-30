59dad67ac25ffaa6c457a6c02ad3c308fecc8222
Alexandr Belyaev <eminis.ru@gmail.com>
2024-01-31 01:35:28 +0300

    RA-09 Create fun openRecipesByCategoryId()

--- app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -1,10 +1,14 @@
 package com.example.recipeapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipeapp.databinding.FragmentListCategoriesBinding
 import data.STUB
@@ -37,5 +41,17 @@ class CategoriesListFragment : Fragment() {
         val adapter = CategoriesListAdapter(STUB.getCategories(), this)
         recyclerView = binding.rvCategories
         recyclerView?.adapter = adapter
+
+        adapter.setOnItemClickListener(object : CategoriesListAdapter.OnItemClickListener {
+            override fun onItemClick() {
+                fun openRecipesByCategoryId(activity: MainActivity) {
+                    activity.supportFragmentManager.commit {
+                        replace<RecipesListFragment>(R.id.mainContainer)
+                        setReorderingAllowed(true)
+                        addToBackStack(null)
+                    }
+                }
+            }
+        })
     }
 }

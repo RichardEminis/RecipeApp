60dcddb57d3ab1da7afbdec13e1cfd6db37026ec
Alexandr Belyaev <eminis.ru@gmail.com>
2024-02-06 00:29:04 +0300

    RA-11 Add fun getCategories() and burgerRecipes List

--- app/src/main/java/data/recipe.kt
+++ app/src/main/java/data/recipe.kt
@@ -4,6 +4,17 @@ import model.Category
 
 object STUB {
 
+    private val burgerRecipes = listOf(
+        "Классический гамбургер",
+        "Чизбургер",
+        "Бургер с грибами и сыром",
+        "Бургер с курицей и авокадо",
+        "бургер с рыбой",
+        "Бургер с беконом",
+        "Веганский бургер",
+        "острый гамбургер"
+    )
+
     private var categories: List<Category> = (listOf(
         Category(0, "Бургеры", "Рецепты всех популярных видов бургеров", "burger.png"),
         Category(1, "Десерты", "Самые вкусные рецепты десертов специально для вас", "dessert.png"),
@@ -14,4 +25,8 @@ object STUB {
     ))
 
     fun getCategories() = categories
+    fun getRecipesByCategoryId(categoryId: Int): List<String>? {
+        return if (categoryId == 0) burgerRecipes
+        else null
+    }
 }

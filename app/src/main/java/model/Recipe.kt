b4dda0b088254bc18b54297bc017697b80a777f6
Alexandr Belyaev <eminis.ru@gmail.com>
2024-02-24 22:23:53 +0300

    RA-12 Implement Parcelize for Recipe

--- app/src/main/java/model/Recipe.kt
+++ app/src/main/java/model/Recipe.kt
@@ -1,9 +1,14 @@
 package model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
+
+@Parcelize
 data class Recipe(
     val id: Int,
     val title: String,
-    val listOfIngredients: List<Ingredient>,
+    val listOfIngredients: @RawValue List<Ingredient>,
     val method: List<String>,
     val imageUrl: String,
-)
+): Parcelable

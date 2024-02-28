7fa2dca0a76cae4fca938d408f835edb443f1370
Alexandr Belyaev <eminis.ru@gmail.com>
2024-02-28 17:05:22 +0300

    RA-13 Add MethodAdapter and Ingredients Adapter

---
+++ app/src/main/java/com/example/recipeapp/IngredientsAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.recipeapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import model.Recipe
+
+class IngredientsAdapter(
+    private val dataSet: List<Recipe>
+): RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}

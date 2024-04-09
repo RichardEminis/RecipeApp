d17de4aba8d568e119585618110209a09b0f1153
Eminis <eminis.ru@gmail.com>
2024-04-09 23:47:27 +0300

    RA-20 Edit

--- app/src/main/java/ui/recipe/RecipeViewModel.kt
+++ app/src/main/java/ui/recipe/RecipeViewModel.kt
@@ -1,14 +1,11 @@
 package ui.recipe
 
 import androidx.lifecycle.ViewModel
-import model.Ingredient
-
-data class RecipeUiState(
-    var id:  Int? = null,
-    var title:  String? = null,
-    var listOfIngredients:  List<Ingredient>? = null,
-    var method :  List<String>? = null,
-    var imageUrl :  String? = null,
-)
+import model.Recipe
 
 class RecipeViewModel : ViewModel()
+
+data class RecipeUiState(
+    var recipe: Recipe? = null,
+    var portionsCount: Int = 0,
+)

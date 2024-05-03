98e6669261cfefa2e8daa0ac7366a969a2668105
Alexandr Belyaev <eminis.ru@gmail.com>
2024-05-03 19:22:44 +0300

    RA-27 Created CategoriesListViewModel

--- app/src/main/java/ui/categories/CategoriesListViewModel.kt
+++ app/src/main/java/ui/categories/CategoriesListViewModel.kt
@@ -1,2 +1,22 @@
 package ui.categories
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import data.STUB
+import model.Category
+
+class CategoriesListViewModel : ViewModel() {
+
+    private val _categoriesList = MutableLiveData<List<Category>>()
+    val categoriesList: LiveData<List<Category>>
+        get() = _categoriesList
+
+    init {
+        loadCategories()
+    }
+
+    private fun loadCategories() {
+        _categoriesList.value = STUB.getCategories()
+    }
+}

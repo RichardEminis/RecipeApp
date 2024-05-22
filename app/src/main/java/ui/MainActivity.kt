73573820de4a0a7b8b4f77cda30e20448558070b
Eminis <eminis.ru@gmail.com>
2024-05-22 23:44:22 +0300

    RA-32 Implemented URLConnection

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -1,13 +1,15 @@
 package ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.example.recipeapp.R
 import com.example.recipeapp.databinding.ActivityMainBinding
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-
     private var _binding: ActivityMainBinding? = null
     private val binding
         get() = _binding
@@ -18,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Log.d("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
+
         binding.favoritesButton.setOnClickListener {
             findNavController(R.id.nav_host_fragment).navigate(R.id.favoritesFragment)
         }
@@ -25,5 +29,22 @@ class MainActivity : AppCompatActivity() {
         binding.categoryButton.setOnClickListener {
             findNavController(R.id.nav_host_fragment).navigate(R.id.categoriesListFragment)
         }
+
+        connection()
+    }
+
+    private fun connection() {
+        val thread = Thread {
+            Log.d("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            Log.d("!!!", "responseCode ${connection.responseCode}")
+            Log.d("!!!", "responseMessage ${connection.responseMessage}")
+            Log.d("!!!", "Body ${connection.inputStream.bufferedReader().readText()}")
+        }
+        thread.start()
     }
 }

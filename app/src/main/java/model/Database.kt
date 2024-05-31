67bf99e70a0d897ca74cd1dcf15f661c1c9a5377
Alexandr Belyaev <eminis.ru@gmail.com>
2024-05-31 15:29:21 +0300

    RA-40 Updated Database.kt

--- app/src/main/java/model/Database.kt
+++ app/src/main/java/model/Database.kt
@@ -6,4 +6,5 @@ import androidx.room.RoomDatabase
 @Database(entities = [Category::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun categoriesDao(): CategoriesDao
+    abstract fun recipesDao(): RecipesDao
 }

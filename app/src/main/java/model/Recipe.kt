4ee1c9a9f8fe8443af733c7205e9cc68d2190d5b
Alexandr Belyaev <eminis.ru@gmail.com>
2024-05-31 15:27:28 +0300

    RA-40 Implemented Dao in dataClass - Recipe

--- app/src/main/java/model/Recipe.kt
+++ app/src/main/java/model/Recipe.kt
@@ -1,15 +1,30 @@
 package model
 
 import android.os.Parcelable
+import androidx.room.Dao
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
 @Parcelize
+@Entity(tableName = "recipe")
 data class Recipe(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val title: String,
     val ingredients: List<Ingredient>,
     val method: List<String>,
     val imageUrl: String,
 ) : Parcelable
+
+@Dao
+interface RecipesDao {
+    @Query("SELECT * FROM recipe")
+    suspend fun getRecipesByCategory(categoryId: Int): List<Recipe>
+
+    @Insert
+    suspend fun insertRecipes(recipes: List<Recipe>)
+}

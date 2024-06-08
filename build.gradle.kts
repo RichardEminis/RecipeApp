7e22bbc389033f6a858b178e1e48805105be7cdc
Alexandr Belyaev <eminis.ru@gmail.com>
2024-06-08 13:50:22 +0300

    RA-45 Implemented Hilt in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidxNavigationSafeArgs) apply false
+    alias(libs.plugins.hiltAndroid) apply false
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.kotlinSerialization)
 }

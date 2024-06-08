9a1f8cda1d5ecea801bab609aca8483d47cd5758
Alexandr Belyaev <eminis.ru@gmail.com>
2024-06-08 12:27:00 +0300

    RA-44 Added RecipesListViewModelFactory

---
+++ app/src/main/java/com/example/recipeapp/di/RecipesListViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.example.recipeapp.di
+
+import com.example.recipeapp.RecipesRepository
+import com.example.recipeapp.ui.recipe.recipeList.RecipesListViewModel
+
+class RecipesListViewModelFactory(
+    private val recipesRepository: RecipesRepository,
+    ): Factory<RecipesListViewModel> {
+
+    override fun create(): RecipesListViewModel {
+        return RecipesListViewModel(recipesRepository)
+    }
+}
